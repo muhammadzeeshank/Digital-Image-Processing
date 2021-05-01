7b5f6eb4d0cd8cc83d92bf71f0389ec5eb8656a5
muhammadzeeshank <zeeshan124go@gmail.com>
2021-05-01 13:43:38 +0500

    initial commit

--- lab6_spatial_filtering/functions.py
+++ lab6_spatial_filtering/functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import random
 from tqdm import tqdm  # for progress bar
 
 
@@ -138,3 +139,22 @@ def maxFilter(img, img1, size):
                     data.append(img[i+y][j+x])
             img1[y][x] = np.max(data)
     return img1
+
+
+def sp_noise(image, prob):
+    '''
+    Add salt and pepper noise to image
+    prob: Probability of the noise
+    '''
+    output = np.zeros(image.shape, np.uint8)
+    thres = 1 - prob
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            rdn = random.random()
+            if rdn < prob:
+                output[i][j] = 0
+            elif rdn > thres:
+                output[i][j] = 255
+            else:
+                output[i][j] = image[i][j]
+    return output
--- lab6_spatial_filtering/task1_L7_min_max_filter.py
+++ lab6_spatial_filtering/task1_L7_min_max_filter.py
@@ -1,18 +1,23 @@
 import numpy as np
 import cv2
-from functions import constructMask, padImage, applyFilter, minFilter
+from functions import constructMask, padImage, medianFilter, sp_noise
 
 
 # **** main ***
 folder = 'images'
-imagename = 'img1b.tif'  # input image name here
+imagename = 'lenna.png'  # input image name here
 imgtoread = folder + '/' + imagename
 # Reading image from drive
 orig_img = cv2.imread(imgtoread, 0)
+# Adding salt and papper noise to image
+noise_img = sp_noise(orig_img, 0.05)
+# constructing mask
 mask, mask_size = constructMask()
-pad_img = padImage(orig_img, mask_size)
-final_img = minFilter(pad_img, orig_img, mask_size)
-# # final image
+# padding image
+pad_img = padImage(noise_img, mask_size)
+# applying median filter
+final_img = medianFilter(pad_img, noise_img, mask_size)
+# final image
 imagetodisplay = folder + '/' + 'result_' + imagename
 cv2.imwrite(imagetodisplay, final_img)
 cv2.waitKey(0)
