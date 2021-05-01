6a03c7dc4e022cbb7ae2d5878a5ee20cb412bb86
muhammadzeeshank <zeeshan124go@gmail.com>
2021-05-01 12:32:38 +0500

    enhanced code

--- lab6_spatial_filtering/functions.py
+++ lab6_spatial_filtering/functions.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2
+from tqdm import tqdm  # for progress bar
 
 
 def constructMask():
     """
-    Purpose: A function that will take input from user and build a required mask
+    Purpose: Takes input from user and build a required mask
     : return: mask, size of mask
     """
     # Taking size and value of mask from user
@@ -48,3 +49,38 @@ def fractionToFloat(value):
     else:
         value = float(value)
     return value
+
+
+def padImage(img1, size):
+    """
+    Purpose: Pads image according to size of mask
+    : param img1: original image
+    : param size: size of mask
+    : return: padded image
+    """
+    # Padding image
+    padsize = int(size/2)
+    img = np.lib.pad(img1, ((padsize, padsize), (padsize, padsize)), mode='constant',
+                     constant_values=(0, 0))
+    return img
+
+
+def applyFilter(img, img1, mask):
+    """
+    Purpose: Applies filter according to mask size and values
+    : param img: the padded image
+    : param img1: the original image
+    : return: convovled image with mask
+    """
+    # convolving mask with image
+    rows, cols = img1.shape
+    img2 = np.zeros([rows, cols])
+    size = mask.shape[0]
+    for y in tqdm(range(rows)):
+        for x in range(cols):
+            val = 0
+            for i in range(size):
+                for j in range(size):
+                    val += img[i + y][j + x] * mask[i][j]
+            img1[y][x] = val
+    return img1
--- lab6_spatial_filtering/task1_L7_min_max_filter.py
+++ lab6_spatial_filtering/task1_L7_min_max_filter.py
@@ -1,53 +1,22 @@
 import numpy as np
 import cv2
-from tqdm import tqdm  # for progress bar
-from functions import constructMask
-
-
-def padImage(img1, size):
-    """
-    : param img1: original image
-    : param size: size of mask
-    : return: padded image
-    """
-    # Padding image
-    padsize = int(size/2)
-    img = np.lib.pad(img1, ((padsize, padsize), (padsize, padsize)), mode='constant',
-                     constant_values=(0, 0))
-    return img
-
-
-def applyFilter(img, img1, mask):
-    """
-    : param img: the padded image
-    : param img1: the original image
-    : return: convovled image with mask
-    """
-    # convolving mask with image
-    rows, cols = img1.shape
-    img2 = np.zeros([rows, cols])
-    size = mask.shape[0]
-    for y in tqdm(range(rows)):
-        for x in range(cols):
-            val = 0
-            for i in range(size):
-                for j in range(size):
-                    val += img[i + y][j + x] * mask[i][j]
-            img1[y][x] = val
-    return img1
+from functions import constructMask, padImage, applyFilter
 
 
 # **** main ***
+folder = 'images'
+imagename = 'img1b.tif'  # input image name here
+imgtoread = folder + '/' + imagename
 # Reading image from drive
-orig_img = cv2.imread('images/img1b.tif', 0)
+orig_img = cv2.imread(imgtoread, 0)
 mask, mask_size = constructMask()
-print(mask)
-# pad_img = padImage(orig_img, mask_size)
+pad_img = padImage(orig_img, mask_size)
 # # # to apply filter by your choice
-# final_img = applyFilter(pad_img, orig_img, mask)
+final_img = applyFilter(pad_img, orig_img, mask)
 # # # to apply median Filter
 # # final_img = medianFilter(pad_img, orig_img, mask_size)
 # # final image
-# cv2.imwrite('img1b.jpg', final_img)
+imagetodisplay = folder + '/' + 'result_' + imagename
+cv2.imwrite(imagetodisplay, final_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
