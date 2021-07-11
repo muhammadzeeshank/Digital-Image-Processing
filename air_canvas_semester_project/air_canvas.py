c53be3b16ad84d36294fed8a345b36a8e78008f8
muhammadzeeshank <zeeshan124go@gmail.com>
2021-07-11 12:12:32 +0500

    initial commit

--- air_canvas_semester_project/air_canvas.py
+++ air_canvas_semester_project/air_canvas.py
@@ -8,16 +8,17 @@ def nothing(x):
     print('')
 
 
-colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255),
+          (0, 165, 255), (0, 0, 0), (255, 255, 255)]
 # creating Trackbars for choosing the color that has to be tracked
 # upper bound
-cv2.createTrackbar('upper hue', 'Color detectors', 153, 255, nothing)
-cv2.createTrackbar('upper sat', 'Color detectors', 255, 255, nothing)
-cv2.createTrackbar('upper val', 'Color detectors', 167, 255, nothing)
+cv2.createTrackbar('upper hue', 'Color detectors', 118, 255, nothing)
+cv2.createTrackbar('upper sat', 'Color detectors', 223, 255, nothing)
+cv2.createTrackbar('upper val', 'Color detectors', 137, 255, nothing)
 # lower bound
-cv2.createTrackbar('lower hue', 'Color detectors', 64, 255, nothing)
-cv2.createTrackbar('lower sat', 'Color detectors', 86, 255, nothing)
-cv2.createTrackbar('lower val', 'Color detectors', 30, 255, nothing)
+cv2.createTrackbar('lower hue', 'Color detectors', 84, 255, nothing)
+cv2.createTrackbar('lower sat', 'Color detectors', 127, 255, nothing)
+cv2.createTrackbar('lower val', 'Color detectors', 73, 255, nothing)
 
 
 cap = cv2.VideoCapture(1)
@@ -32,6 +33,8 @@ else:
 while ret:
     # capturing video from camera
     ret, frame = cap.read()
+    # fliping arround x-axis
+    frame = cv2.flip(frame, 1)
 
     # converting BGR image to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -47,17 +50,28 @@ while ret:
     lowerb = np.array([l_hue, l_sat, l_val])
     # making mask
     mask = cv2.inRange(hsv, lowerb, upperb)
+    # finding contours
+    contours = cv2.findContours(
+        mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+
+    # finding contour with max area
+    if len(contours) > 0:
+        cont_max_area = max(contours, key=cv2.contourArea)
+        center, radius = cv2.minEnclosingCircle(cont_max_area)
+        # drawing circle with center at the center of contour
+        cv2.circle(frame, (int(center[0]), int(
+            center[1])), int(radius), (0, 255, 0), 3)
 
     result = cv2.bitwise_and(frame, frame, mask=mask)
     # Adding the colour buttons to the live frame for colour access
     frame = cv2.rectangle(frame, (40, 0), (140, 65), (122, 122, 122), -1)
     y_and_r = 25
-    cv2.circle(frame, (200, y_and_r), y_and_r, (255, 0, 0), -1)
-    cv2.circle(frame, (275, y_and_r), y_and_r, (0, 255, 0), -1)
-    cv2.circle(frame, (350, y_and_r), y_and_r, (0, 0, 255), -1)
-    cv2.circle(frame, (425, y_and_r), y_and_r, (0, 165, 255), -1)
-    cv2.circle(frame, (500, y_and_r), y_and_r, (0, 0, 0), -1)
-    cv2.circle(frame, (575, y_and_r), y_and_r, (255, 255, 255), -1)
+    cv2.circle(frame, (200, y_and_r), y_and_r, colors[0], -1)
+    cv2.circle(frame, (275, y_and_r), y_and_r, colors[1], -1)
+    cv2.circle(frame, (350, y_and_r), y_and_r, colors[2], -1)
+    cv2.circle(frame, (425, y_and_r), y_and_r, colors[3], -1)
+    cv2.circle(frame, (500, y_and_r), y_and_r, colors[4], -1)
+    cv2.circle(frame, (575, y_and_r), y_and_r, colors[5], -1)
 
     cv2.putText(frame, "CLEAR ALL", (49, 33),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
